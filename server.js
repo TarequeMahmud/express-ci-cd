2c1647f32f8fba435a962f498d6d39d498bf45e1
TarequeMahmud <107052189+TarequeMahmud@users.noreply.github.com>
2025-05-26 12:48:20 +0600

    feat: Add ESLint and Prettier for code quality #deploy_now

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello from CI/CD Express App!");
+app.get('/', (req, res) => {
+  res.send('Hello from CI/CD Express App!');
 });
 
 if (require.main === module) {
